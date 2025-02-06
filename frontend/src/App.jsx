e74a38b30004888a88d3ddbdc89d37cdf395af2a
Deepy25agr <agrdeepika25@gmail.com>
2025-02-06 22:50:53 +0530

    home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,9 +7,12 @@ import Navbar from './components/shared/Navbar';
 import Signup from './components/auth/Signup';
 import Login from './components/auth/Login';
 import Browse from './components/Browse';
-
+import Home from './components/Home';
 import Profile from './components/profile';
 import Contact from './components/contact';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './index.css';
 function App() {
   return (
     <>
@@ -17,7 +20,7 @@ function App() {
       <Navbar />
       <div className="flex-grow">
       <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/Home" element={<Home/>} />
       <Route path="/jobs" element={<h1>Jobs Page</h1>} />
         <Route path="/browse" element={<Browse/>} />
         <Route path="/signup" element={<Signup />} />
@@ -32,5 +35,5 @@ function App() {
     </>
   );
 }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+
 export default App;
